c82c36909221ea0660b8a08b17d2c0b7674fe1b1
mars7x3 <m.ysakov.jcc@gmail.com>
2025-10-02 21:10:32 +0600

    user crud

--- app/api/serializers/auth.py
+++ app/api/serializers/auth.py
@@ -1,11 +1,22 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
+from db.enums import UserStatus
+
+
+# class UserLoginSerializer(TokenObtainPairSerializer):
+#     @classmethod
+#     def get_token(cls, user):
+#         token = super().get_token(user)
+#
+#         token['status'] = user.status
+#
+#         return token
 
 class UserLoginSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['status'] = self.user.status
+        if self.user.status == UserStatus.STAFF:
+            data['role'] = self.user.staff_profile.role
 
-        token['status'] = user.status
-
-        return token
+        return data
